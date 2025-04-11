073fb98a90789cdd55c4654351787108676558b5
Dr-Herend <ihara.hypnos@gmail.com>
2025-04-12 01:36:16 +0900

    Add BP35C2 support

--- custom_components/b_route_meter/adapter_factory.py
+++ custom_components/b_route_meter/adapter_factory.py
@@ -4,7 +4,7 @@ from typing import Type
 
 from .adapter_interface import AdapterInterface
 from .adapters.bp35a1 import BP35A1Adapter
-
+from .adapters.bp35c2 import BP35C2Adapter
 
 class AdapterFactory:
     """Factory class for creating B-route adapters."""
